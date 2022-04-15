b3d1bb682e7a514654c0b7dc14037c4e0176c95c
Oleg Kolovanov <olegkolovanov@yandex.ru>
2022-04-15 16:35:43 +0300

    13.13

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/users', require('./routes/users'));
-app.use('/', require('./routes/cards'));
+app.use('/cards', require('./routes/cards'));
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Запрашиваемая страница отсутсвует' });
