28c6438aa651b364d545a1709c79edbfc36bbb0d
Oleg Kolovanov <olegkolovanov@yandex.ru>
2022-04-15 16:27:33 +0300

    13.12

--- controllers/users.js
+++ controllers/users.js
@@ -34,7 +34,7 @@ module.exports.createUsers = (req, res) => {
 }
 
 module.exports.createMe = (req, res) => {
-  User.findByIdAndUpdate(req.params.id, { name, about }, updateParams)
+  User.findByIdAndUpdate(req.params.userId, { name, about }, updateParams)
   .then((user) => ((!user)
   ? res.status(404).send({ message: 'Пользователь не найден' })
   : res.send({ data: user })))
@@ -44,7 +44,7 @@ module.exports.createMe = (req, res) => {
 };
 
 module.exports.createUserAvatar = (req, res) => {
-  User.findByIdAndUpdate(req.params.id, { avatar }, { new: true }, updateParams)
+  User.findByIdAndUpdate(req.params.userId, { avatar }, { new: true }, updateParams)
   .then((user) => ((!user)
   ? res.status(404).send({ message: 'Пользователь не найден' })
   : res.send({ data: user })))
