85fa89e789aadd20a13ba6364b7053fc4dfa8f09
Oleg Kolovanov <olegkolovanov@yandex.ru>
2022-04-26 16:43:10 +0300

    14.8

--- app.js
+++ app.js
@@ -35,10 +35,11 @@ app.post('/signup', celebrate({
 }), createUsers);
 
 app.use(auth);
+
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use('*', (req, res, next) => {
+app.use((req, res, next) => {
   next(new NotFoundErr('Страница не найдена'));
 });
 
--- controllers/users.js
+++ controllers/users.js
@@ -157,7 +157,8 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-        });
+        })
+        .end();
     })
     .catch(() => {
       next(new Unauthorized('Неверно указана электронная почта или пaроль'));
