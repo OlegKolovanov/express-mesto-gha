d56fab9d6fffdc047fe1f88e8f343ecea0c78d3f
Oleg Kolovanov <olegkolovanov@yandex.ru>
2022-04-26 14:23:35 +0300

    14.3

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use((req, res, next) => {
-  next(new NotFoundErr('Запрашиемый ресур не найден'));
+app.use('*', (req, res, next) => {
+  next(new NotFoundErr('Страница не найдена'));
 });
 
 app.use(errors());
