216c8e43d1587481b6679e43ed25175e3e61c237
Oleg Kolovanov <olegkolovanov@yandex.ru>
2022-04-26 19:41:58 +0300

    14.14

--- controllers/users.js
+++ controllers/users.js
@@ -147,13 +147,13 @@ module.exports.login = (req, res, next) => {
       // создадим токен
       const token = jwt.sign({ _id: user._id }, 'super-strong-secret', { expiresIn: '7d' });
 
-      // res
-      //   .cookie('jwt', token, {
-      //     maxAge: 3600000 * 24 * 7,
-      //     httpOnly: true,
-      //   })
-      //   .end();
-      res.send({ token });
+      return res
+        .cookie('token', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+          sameSite: true,
+        })
+        .json({ message: 'Успешная авторизация' });
     })
     .catch(() => {
       next(new Unauthorized('Неверно указана электронная почта или пaроль'));
