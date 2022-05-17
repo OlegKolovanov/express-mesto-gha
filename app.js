41e1c487d8720f9528055b403350a3bcda7c8f7b
Oleg Kolovanov <olegkolovanov@yandex.ru>
2022-05-17 23:37:39 +0300

    14.44

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { login, createUsers } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const handleError = require('./middlewares/handleError');
 const NotFoundErr = require('./errors/NotFoundErr');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -15,6 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/mestodb', () => {
   console.log('Yes');
 });
+
+app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -37,6 +40,8 @@ app.use(auth);
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
+app.use(errorLogger);
 
 app.use('*', (req, res, next) => {
   next(new NotFoundErr('Страница не найдена'));
