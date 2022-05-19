a4485c835da8c4ecb0e666fbc093324db1ad7271
Oleg Kolovanov <olegkolovanov@yandex.ru>
2022-05-19 14:12:52 +0300

    0003

--- controllers/users.js
+++ controllers/users.js
@@ -146,7 +146,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: true,
         });
-      res.status(200).send({ token, user, message: 'Пользователь успешно зарегистрирован' });
+      res.status(200).send({ token });
     })
     .catch(() => {
       next(new Unauthorized('Неверно указана электронная почта или пaроль'));
