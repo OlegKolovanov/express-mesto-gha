1917d190d9c76d03a7ad812833743f235038fab1
Oleg Kolovanov <olegkolovanov@yandex.ru>
2022-05-20 14:40:34 +0300

    0013

--- controllers/cards.js
+++ controllers/cards.js
@@ -11,9 +11,8 @@ module.exports.getCard = (req, res, next) => {
 
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
-  const owner = req.user._id;
 
-  Card.create({ name, link, owner })
+  Card.create({ name, link, owner: req.user._id })
     .then((cards) => res.send({ data: cards }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
